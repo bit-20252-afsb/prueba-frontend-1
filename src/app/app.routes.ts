a387c81a3e8f044065f8fccccfa1105c750772f7
AndresFSerranoPC <andresfserrano1@gmail.com>
2025-07-24 21:46:38 -0500

    feat protection with login and register routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,16 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Register } from './pages/register/register';
 import { Login } from './pages/login/login';
+import { authGuardGuard } from './guards/auth-guard-guard';
 
 export const routes: Routes = [
     {
-        path: 'home', component: Home,
+        path: 'home', component: Home, // Crear una redireccion a login en caso de que no se tenga la sesion activa
     },
     {
-        path: 'register', component: Register,
+        path: 'register', component: Register, canActivate: [authGuardGuard],
     },
     {
-        path: 'login', component: Login,
+        path: 'login', component: Login, canActivate: [authGuardGuard]
     }
 ];
