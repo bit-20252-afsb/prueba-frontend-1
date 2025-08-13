8c8ab891fe71e9c545da9093b50ca919c8267d5b
AndresFSerranoPC <andresfserrano1@gmail.com>
2025-08-12 20:52:34 -0500

    feat: adding basic structure for service testing

--- src/app/services/auth/auth-service.spec.ts
+++ src/app/services/auth/auth-service.spec.ts
@@ -1,13 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth-service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[AuthService]
+    });
     service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    sessionStorage.clear();
+    httpMock.verify();
   });
 
   it('should be created', () => {
